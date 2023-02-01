e59a7d34bee34a7bf3cb5d673aa1a8d62af68151
HJoonKwon <dsazz801@gmail.com>
2023-02-02 00:05:13 +0900

    gda scratch

---
+++ ml_algorithms/gda.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class GaussianDiscriminantAnalysis():
+
+    def __init__(self):
+        self.theta: np.ndarray
+
+    def fit(self, train_X: np.ndarray, train_Y: np.ndarray):
+
+        # train_X : (m, n)
+        # train_Y : (m, )
+        # phi, mu0, mu1, Cov
+
+        m, n = train_X.shape[0], train_X.shape[1]
+
+        self.theta = np.zeros(n+1)
+
+        y1 = np.sum(train_Y)
+        phi = 1/m * y1 # real number
+        mu1 = np.sum(train_X[train_Y==1], axis=0) / y1
+        mu0 = np.sum(train_X[train_Y==0], axis=0) / (m-y1)
+        cov = 1/m * ((train_X - mu0).T @ (train_X - mu0) + (train_X - mu1).T @ (train_X - mu1))
+
+        assert mu1.shape == (train_X.shape[1],)
+        assert mu0.shape == (train_X.shape[1],)
+        assert cov.shape == (train_X.shape[1], train_X.shape[1])
+
+        cov_inv = np.linalg.inv(cov)
+        self.theta[0] = 0.5 * (mu0 + mu1).T.dot(cov_inv).dot(mu0 - mu1) - np.log((1-phi)/phi)
+        self.theta[1:] = cov_inv.dot(mu1 - mu0)
+        return self.theta
+
+
+    def predict(self):
+        pass
+
+
+if __name__ == "__main__":
+    # for test
+    model = GaussianDiscriminantAnalysis()
+    X = np.zeros((6, 5))
+    Y = np.array([0, 1, 0, 1, 0, 1])
+    model.fit(X, Y)
